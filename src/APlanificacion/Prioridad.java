888de5bb90c0a3ec442c96fc41761e97c3d2e3cb
Javier Flores <javierfh1404@gmail.com>
2016-02-29 17:35:37 -0600

    Prioridad funcionando

--- src/APlanificacion/Prioridad.java
+++ src/APlanificacion/Prioridad.java
@@ -16,6 +16,8 @@ import static simulador.FXMLDocumentController.bandera;
 import static simulador.FXMLDocumentController.cpu;
 import static simulador.FXMLDocumentController.colaEspera;
 import static simulador.FXMLDocumentController.data;
+import static simulador.FXMLDocumentController.data1;
+import static simulador.FXMLDocumentController.data2;
 import simulador.Row;
 /**
  *
@@ -29,32 +31,45 @@ public class Prioridad extends Thread{
     int thick = 1;
     int numProceso = 0;
     double penalizacion = 0;
-    TableView tableV;
+    TableView tableV,tableCPU,tableSalida;
     TextArea txtTe,txtTr,txtP;
     public Prioridad(){
         
     }
-   public Prioridad(TableView tableV,TextArea txtTe,TextArea  txtTr,TextArea txtP){
+   public Prioridad(TableView tableV,TableView tableCPU, TableView tableSalida,TextArea txtTe,TextArea  txtTr,TextArea txtP){
        this.tableV = tableV;
+       this.tableCPU = tableCPU;
+       this.tableSalida = tableSalida;
        this.txtP = txtP;
        this.txtTe = txtTe;
        this.txtTr = txtTr;
    }
+   //Pone los elementos en tabla salida
    private void setTabla(String pri,String tL,String tR,String te,String tRespuesta,String penali,String or){
        Row aux = new Row(tL,tR, pri);  
        
-       for (int i = 0; i < data.size(); i++) {
+
             
-            if(pri.equals(data.get(i).getPrioridad()) && tL.equals(data.get(i).getTiempoLlegada()) && 
-                    tR.equals(data.get(i).getTiempoRequerido())){
-               
                 aux.setTe(te);
                 aux.setTr(tRespuesta);
                 aux.setP(penali);
                 aux.setOrden(or);
-                data.remove(i);
-                data.add(i,aux);
+                data2.add(aux);
+                tableSalida.setItems(data2);
+              
+        
+   }
+     private void eliminar(String tL,String tR, String Pri){
+       Row aux = new Row(tL,tR);  
+       
+       for (int i = 0; i < data.size(); i++) {
+            
+            if(tL.equals(data.get(i).getTiempoLlegada()) && 
+                    tR.equals(data.get(i).getTiempoRequerido() ) && Pri.equals(data.get(i).getPrioridad())){
                
+      
+                data.remove(i);
+                
                 return;
             }
             
@@ -71,18 +86,29 @@ public class Prioridad extends Thread{
        return te+"";
    }
    
+     private void setTablaCPU(String tL, String th, String prioridad) {
+         tableCPU.getItems().clear();
+         data1.clear();
+        data1.add(new Row(tL + "", th + "", prioridad + ""));
+        tableCPU.setItems(data1);
+    }
+
     @Override
     public void run(){
-        
+        String a = " ", b= " ",c=" ";
         do{
             if(!colaEspera.isEmpty()){   
+                
                 numProceso++;
                 String aux = colaEspera.remove(0);
+                
                 String[] split = aux.split(":");
                 thick = Integer.parseInt(split[2]); //tiempo requerido
                 
                 int tiempoL = Integer.parseInt(split[1]); //tiempo llegada
-       
+                a = split[0];
+                b = split[1];
+                c = split[2];
                 trespuesta = t - tiempoL + thick;
                 getTiempoEspera(tiempoL);
                 
@@ -92,16 +118,27 @@ public class Prioridad extends Thread{
                 sumTr += trespuesta;
                 sumPe += penalizacion;
                 sumTe += te;
-                setTabla(split[0],split[1],split[2],te+"",trespuesta+"",penalizacion+"",numProceso+"");
+              
                 t = t+ thick;
                 cpu.add(aux );
-      
-            }             
-            try {
-                Thread.sleep(1000 * thick);
+                setTablaCPU(b, c, a);
+                eliminar(b,c,a);
+                 try {
+                Thread.sleep(2000 * thick);
+                 tableCPU.getItems().clear();
+                 setTabla(a,b,c,te+"",trespuesta+"",penalizacion+"",numProceso+"");
             } catch (InterruptedException ex) {
                 Logger.getLogger(Prioridad.class.getName()).log(Level.SEVERE, null, ex);
             }      
+            }else{
+                 try {
+                Thread.sleep(2000 * thick);
+              
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Prioridad.class.getName()).log(Level.SEVERE, null, ex);
+            }      
+            }             
+           
             
         }while(bandera);
         System.out.println("Hilo terminado");
